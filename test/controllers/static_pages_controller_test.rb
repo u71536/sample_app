109a92093375d7da54af2741eca8862057b75878
Djivan <u71536@gmail.com>
2017-05-09 11:12:55 +0000

    Add a Static Pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,8 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test "should get help" do
+    get static_pages_help_url
     assert_response :success
   end
 
